caccedcb34b0e2a54971e42ff1efb68913939b43
Paweł <pawel_gnat@o2.pl>
2022-11-21 21:12:15 +0100

    fix: js import file

--- js/components/_theme.js
+++ js/components/_theme.js
@@ -4,49 +4,10 @@ let theme = localStorage.getItem('theme')
 
 themeInputs.forEach(input =>
 	input.addEventListener('click', e => {
-		// switch (e.target.id) {
-		// 	case 'theme-1':
-		// 		themeSwitcher.style.translate = '0'
-		// 		document.querySelector('body').classList.remove('theme2')
-		// 		document.querySelector('body').classList.remove('theme3')
-		// 		theme = 'theme-1'
-		// 		break
-		// 	case 'theme-2':
-		// 		themeSwitcher.style.translate = '2.1rem'
-		// 		document.querySelector('body').classList.add('theme2')
-		// 		document.querySelector('body').classList.remove('theme3')
-		// 		theme = 'theme-2'
-		// 		break
-		// 	case 'theme-3':
-		// 		themeSwitcher.style.translate = '4.3rem'
-		// 		document.querySelector('body').classList.remove('theme2')
-		// 		document.querySelector('body').classList.add('theme3')
-		// 		theme = 'theme-3'
-		// 		break
-		// }
-
 		checkTheme(e.target.id)
-
-		// localStorage.setItem('theme', theme)
 		localStorage.setItem('theme', e.target.id)
 	})
 )
-
-// function checkTheme() {
-// 	if (theme === 'theme-1') {
-// 		return (themeSwitcher.style.translate = '0')
-// 	}
-
-// 	if (theme === 'theme-2') {
-// 		themeSwitcher.style.translate = '2.1rem'
-// 		return document.querySelector('body').classList.add('theme2')
-// 	}
-
-// 	if (theme === 'theme-3' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-// 		themeSwitcher.style.translate = '4.3rem'
-// 		return document.querySelector('body').classList.add('theme3')
-// 	}
-// }
 
 function checkTheme(theme) {
 	if (theme === 'theme-1') {
--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-import  "./components/_theme.js"
+import * as theme from './components/_theme.js'
 
+const resultScreen = document.querySelector('.screen__result')
+const calcButtons = document.querySelectorAll('.button--number')
+const optionsButtons = document.querySelectorAll('.button--options')
+const resultButton = document.querySelector('.button--result')
+
+function showResult(result = 0) {
+	resultScreen.textContent = result
+}
+showResult()
